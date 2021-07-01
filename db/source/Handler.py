a7706e6f25c12e43d17ed21a228348cdac8ac786
PDA-Khacer <59501767_PDA_Khacer@users.noreply.github.com>
2021-07-01 23:30:04 +0700

    update code db, add and read data basic

--- db/source/Handler.py
+++ db/source/Handler.py
@@ -3,10 +3,13 @@ import os
 from db.source.Const import TABLE_NAME
 from db.source.Const import MAX_RECORDS
 
+
 class DBHandler:
     def __init__(self):
         self.__allTable = dict()
         self.__allFileData = []
+        self.__cacheData = dict()
+        self.__keyData = []
 
     def selectFile(self, key1):
         self.__readFileTables()
@@ -17,7 +20,15 @@ class DBHandler:
         else:
             self.__writeTableToFile(key1)
             return key1 + "_1"
-        
+
+    def readTable(self, table):
+        self.__getAllFileData()
+        for file in self.__allFileData:
+            if table in file:
+                dataFile = self.__readFile(file)
+                self.__toDict(dataFile)
+        return self.__cacheData
+
     # private method
     def __readFileTables(self):
         try:
@@ -30,11 +41,11 @@ class DBHandler:
             print(r'[ERR] db\source\Handler.py line:27 ', e)
         finally:
             f.close()
-        
+
     def __writeTableToFile(self, key):
         try:
             f = open("db/data/" + TABLE_NAME, "a", encoding="utf-8")
-            f.writelines('\n'+ key + ' : 0')
+            f.writelines('\n' + key + ' : 0')
         except Exception as e:
             print(r'[ERR] IO Exception')
             print(r'[ERR] db\source\Handler.py line:37 ', e)
@@ -43,7 +54,7 @@ class DBHandler:
 
     def __getAllFileData(self):
         self.__allFileData = os.listdir('db/data')
-    
+
     def __getNumberOfFile(self, totalCount):
         try:
             return int(int(totalCount) / MAX_RECORDS) + 1
@@ -51,3 +62,48 @@ class DBHandler:
             print(r'[ERR] total count not int format')
             print(r'[ERR] db\source\Handler.py line:52 ', e)
             return -1
+
+    def writeItem(self, table, key, data):
+        nameFile = self.selectFile(table)
+        try:
+            f = open('db/data/'+nameFile, 'a')
+            f.write(self.__hash("<" + str(key) + ":" + str(data) + ">"))
+        except Exception as e:
+            print(r'[ERR] Dir not exits')
+            print(r'[ERR] db\source\Handler.py line:62 ', e)
+
+    def __hash(self, string):
+        re = ''
+        for item in string:
+            re += str(ord(item)+1234)
+        return re
+
+    def __readFile(self, file):
+        try:
+            re = ""
+            with open('db/data/' + file, "rb") as f:
+                while (byte := f.read(4)):
+                    re += str(chr(int(byte)-1234))
+                f.close()
+        except Exception as e:
+            print(r'[ERR] File not exit')
+        return re
+
+    def __toDict(self, string):
+        i = 0
+        s = 1
+        key = ""
+        value = ""
+        flag = -1
+        while i < len(string):
+            if string[i] == ':' and flag != 1:
+                key = string[s:i]
+                self.__keyData.append(key)
+                flag = 1
+                s = i + 1
+            if string[i] == ">":
+                value = string[s:i]
+                self.__cacheData[key] = value
+                flag = -1
+                s = i + 2
+            i += 1
--- test/HandlerFile_test.py
+++ test/HandlerFile_test.py
@@ -1,7 +1,18 @@
+
+import json
 import sys
 
 sys.path.insert(1, '.')
 
 from db.source.Handler import DBHandler
 
+
 print(DBHandler().selectFile("topic"))
+dbIns = DBHandler()
+
+item = dict(name="abc", c="nnn")
+# print(json.dumps(item))
+# dbIns.writeItem("topic", 1, json.dumps(item))
+rows = dbIns.readTable("topic")
+print(json.loads(rows["1"])["name"])
+
