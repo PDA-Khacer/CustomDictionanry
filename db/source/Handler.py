13e859005ac133368cf12abd468154c42488c6e1
PDA-Khacer <59501767_PDA_Khacer@users.noreply.github.com>
2021-07-03 00:04:33 +0700

    complete basic method db get/update/del

--- db/source/Handler.py
+++ db/source/Handler.py
@@ -2,6 +2,7 @@ import os
 
 from db.source.Const import TABLE_NAME
 from db.source.Const import MAX_RECORDS
+from db.source.Const import HIGH_MODE
 
 
 class DBHandler:
@@ -10,6 +11,9 @@ class DBHandler:
         self.__allFileData = []
         self.__cacheData = dict()
         self.__keyData = []
+        self.__tableCache = ""
+        self.__currentIndex = -1
+        self.__mode = 0
 
     def selectFile(self, key1):
         self.__readFileTables()
@@ -27,15 +31,103 @@ class DBHandler:
             if table in file:
                 dataFile = self.__readFile(file)
                 self.__toDict(dataFile)
+        self.__tableCache = table
         return self.__cacheData
 
+    def writeItem(self, table, key, data):
+        if type(key) != str:
+            print(r'[ERR] db\source\Handler.py line:36 ',
+                  "Key must be a string")
+            return -1
+        nameFile = self.selectFile(table)
+        if table != self.__tableCache:
+            self.readTable(table)
+        if any(item == key for item in self.__keyData):
+            print('key is existed')
+            return -1
+
+        try:
+            f = open('db/data/'+nameFile, 'a')
+            del nameFile
+            f.write(self.__hash(
+                "<" + str(self.__allTable[table] + 1) + "@" + str(key) + ":" + str(data) + ">"))
+            self.__incTotalCount(table)
+        except Exception as e:
+            print(r'[ERR] Dir not exits')
+            print(r'[ERR] db\source\Handler.py line:50 ', e)
+        finally:
+            f.close()
+
+    def checkKeyExists(self, table, key):
+        if type(key) != str:
+            print(r'[ERR] db\source\Handler.py line:54 ',
+                  "Key must be a string")
+            return -1
+        if table != self.__tableCache:
+            self.readTable(table)
+        if any(item == key for item in self.__keyData):
+            print('key is existed')
+            return True
+        return False
+
+    def getItem(self, table, key):
+        """ Get item in table with key """
+        if HIGH_MODE == self.__mode:
+            # TODO: use thread
+            pass
+        else:
+            self.readTable(table)
+            try:
+                self.__currentIndex = int(self.__cacheData[key]["index"])
+                return self.__cacheData[key]
+            except Exception as e:
+                print(r'[ERR] no item in dict')
+                return None
+
+    def updateItem(self, table, key, value):
+        if value == None:
+            value = "{}"
+        if HIGH_MODE == self.__mode:
+            # TODO: use thread
+            pass
+        else:
+            oldValue = self.getItem(table, key)['value']
+            nameFile = table+"_"+str(self.__getNumberOfFile(self.__currentIndex))
+            # replace 
+            newData = "<{index}@{key}:{value}>".format(index=self.__currentIndex, key=key, value=value)
+            oldData = "<{index}@{key}:{value}>".format(index=self.__currentIndex, key=key, value=oldValue)
+            newData = self.__readFile(nameFile).replace(oldData, newData)
+            self.__reWriteFile(nameFile, newData)
+        return True
+
+    def deleteitem(self, table, key):
+        if HIGH_MODE == self.__mode:
+            # TODO: use thread
+            pass
+        else:
+            oldValue = self.getItem(table, key)['value']
+            nameFile = table+"_"+str(self.__getNumberOfFile(self.__currentIndex))
+            oldData = "<{index}@{key}:{value}>".format(index=self.__currentIndex, key=key, value=oldValue)
+            newData = self.__readFile(nameFile).replace(oldData, "")
+            self.__reWriteFile(nameFile, newData)
+            self.__descTotalCount(table)
+        return True
+
+    # TODO: work with  multiple data
+    # TODO: get page data
+    # TODO: clear all data
+    # TODO: backup
+    # TODO: trans
+    # TODO: lock
+
     # private method
-    def __readFileTables(self):
+
+    def __readFileTables(self): 
         try:
             f = open("db/data/" + TABLE_NAME, "r", encoding="utf-8")
             for line in f:
                 line = line.replace('\n', '').split(' ')
-                self.__allTable[line[0]] = line[2]
+                self.__allTable[line[0]] = int(line[2])
         except Exception as e:
             print(r'[ERR] IO Exception')
             print(r'[ERR] db\source\Handler.py line:27 ', e)
@@ -45,7 +137,7 @@ class DBHandler:
     def __writeTableToFile(self, key):
         try:
             f = open("db/data/" + TABLE_NAME, "a", encoding="utf-8")
-            f.writelines('\n' + key + ' : 0')
+            f.writelines('\n' + str(key) + ' : 0')
         except Exception as e:
             print(r'[ERR] IO Exception')
             print(r'[ERR] db\source\Handler.py line:37 ', e)
@@ -63,18 +155,15 @@ class DBHandler:
             print(r'[ERR] db\source\Handler.py line:52 ', e)
             return -1
 
-    def writeItem(self, table, key, data):
-        nameFile = self.selectFile(table)
-        try:
-            f = open('db/data/'+nameFile, 'a')
-            f.write(self.__hash("<" + str(key) + ":" + str(data) + ">"))
-        except Exception as e:
-            print(r'[ERR] Dir not exits')
-            print(r'[ERR] db\source\Handler.py line:62 ', e)
-
     def __hash(self, string):
         re = ''
         for item in string:
+            re += str(ord(item)+1234)
+        return re
+
+    def __hashData(self, index, key, value):
+        re = ''
+        for item in "<{index}@{key}:{value}>".format(index=index, key=key, value=value):
             re += str(ord(item)+1234)
         return re
 
@@ -89,7 +178,19 @@ class DBHandler:
             print(r'[ERR] File not exit')
         return re
 
+    def __reWriteFile(self, file, data):
+        try:
+            f = open('db/data/'+file, 'w')
+            f.write(self.__hash(data))
+        except Exception as e:
+            print(r'[ERR] file not existed !')
+            print(r'[ERR] db\source\Handler.py line:96 ', e)
+        finally:
+            f.close()
+
     def __toDict(self, string):
+        # TODO: add function load cache
+        self.__cacheData = dict()
         i = 0
         s = 1
         key = ""
@@ -97,13 +198,46 @@ class DBHandler:
         flag = -1
         while i < len(string):
             if string[i] == ':' and flag != 1:
-                key = string[s:i]
+                keyAndIndex = string[s:i].split("@")
+                key = keyAndIndex[1]
+                self.__currentIndex = int(keyAndIndex[0])
                 self.__keyData.append(key)
                 flag = 1
                 s = i + 1
             if string[i] == ">":
                 value = string[s:i]
-                self.__cacheData[key] = value
+                self.__cacheData[key] = dict(index=keyAndIndex[0], value=value)
                 flag = -1
                 s = i + 2
             i += 1
+
+    def __incTotalCount(self, table):
+        self.__readFileTables()
+        self.__allTable[table] += 1
+        try:
+            f = open("db/data/" + TABLE_NAME, "w", encoding="utf-8")
+            for key in self.__allTable:
+                f.write(str(key) + ' : ' + str(self.__allTable[key]) + '\n')
+        except Exception as e:
+            print(r'[ERR] IO Exception')
+            print(r'[ERR] db\source\Handler.py line:27 ', e)
+        finally:
+            f.close()
+
+    def __descTotalCount(self, table):
+        self.__readFileTables()
+        self.__allTable[table] -= 1
+        try:
+            f = open("db/data/" + TABLE_NAME, "w", encoding="utf-8")
+            for key in self.__allTable:
+                f.write(str(key) + ' : ' + str(self.__allTable[key]) + '\n')
+        except Exception as e:
+            print(r'[ERR] IO Exception')
+            print(r'[ERR] db\source\Handler.py line:27 ', e)
+        finally:
+            f.close()
+    
+    def debugData(self, table):
+        pass
+    
+    # TODO: func re index table
--- test/HandlerFile_test.py
+++ test/HandlerFile_test.py
@@ -1,4 +1,3 @@
-
 import json
 import sys
 
@@ -10,9 +9,24 @@ from db.source.Handler import DBHandler
 print(DBHandler().selectFile("topic"))
 dbIns = DBHandler()
 
-item = dict(name="abc", c="nnn")
+# item = dict(name="nnn", c="nnn")
 # print(json.dumps(item))
-# dbIns.writeItem("topic", 1, json.dumps(item))
-rows = dbIns.readTable("topic")
-print(json.loads(rows["1"])["name"])
+# dbIns.writeItem("topic", "6", json.dumps(item))
 
+# ============================ get all data
+# rows = dbIns.readTable("topic")
+# print("rows : ",rows)
+
+# ============================ get Item
+# print(json.loads(rows["2"])["name"])
+# print("item", dbIns.getItem("topic", "4"))
+
+# ============================ update 
+# item = dict(name="update", c="nnn")
+# dbIns.updateItem("topic","3", json.dumps(item))
+
+# ============================ delete
+dbIns.deleteitem("topic","6")
+
+rows = dbIns.readTable("topic")
+print("rows : ",rows)
